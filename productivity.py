0642ea870e2d6ef319cd613bc3a165e9ee6adde2
fuzesmonk <55888489+fuzesmonk@users.noreply.github.com>
2023-04-25 16:17:23 -0700

    wheeeee

--- productivity.py
+++ productivity.py
@@ -1,15 +1,32 @@
+import subprocess
+import pkg_resources
+import sys
+
+needed_packages = 'python-vlc'
+
+
+def checker(needed_packages):
+    required = {needed_packages}
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
+
+    if missing:
+        python = sys.executable
+        subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+
+checker(needed_packages)
+
 import os
 import random
 import time
 import vlc
 
-
 reward_chances = {
     'DCS': 0.00000001,
-    '5 Min Break': 0.5,
+    '10 Min Break': 0.5,
     '10 Minutes on Youtube': 0.1,
     '1 Crucible Match on D2': 0.05,
-    'Eat snack or roll again' : 0.05,
+    'Eat snack or roll again': 0.05,
     '10 Minutes of anything': 0.05,
     '10 Minutes of Turing Complete': 0.05,
     '10 Minutes of Coding or Roll Again': 0.05,
@@ -17,23 +34,39 @@ reward_chances = {
     'Two Spins': 0.049999999
 }
 
+
 def spin():
     return random.choices(list(reward_chances.keys()), list(reward_chances.values()))[0]
 
+def ready_check():
+    done = True
+    while done:
+        break_check = input("Ready to start next round? Y/N: ")
+        if break_check == "Y":
+            done = False
+        else:
+            time.sleep(600)
+
+
 def main():
-    media = vlc.MediaPlayer('bells.wav')
+    time.sleep(1200)
+    media = vlc.MediaPlayer("C:\Projects\Productivity-Script\churchbells.wav")
     media.play()
     time.sleep(10)
     spin_result = spin()
     print(spin_result)
-    if (spin_result != 'nothing'):
+    if spin_result != 'nothing':
         with open('rewards.txt', 'a') as f:
             f.write(spin_result + '\n')
-    if (spin_result == 'Two Spins'):
-            spin()
-            spin()
-    time.sleep(1200)
+    if spin_result == 'Two Spins':
+        spin()
+        spin()
+
 
 if __name__ == '__main__':
-    while(True):
+    while True:
         main()
+#   Check if break is done
+        ready_check()
+
+
